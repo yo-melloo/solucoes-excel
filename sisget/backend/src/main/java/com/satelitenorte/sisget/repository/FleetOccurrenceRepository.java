6987e9a3a4af8f4d48bed96fec1895acc53f7586
Gustavo Mello <oliver7mello@gmail.com>
2026-04-17 11:37:12 -0300

    chore: remover sisget para repositório dedicado

--- sisget/backend/src/main/java/com/satelitenorte/sisget/repository/FleetOccurrenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.satelitenorte.sisget.repository;
-
-import com.satelitenorte.sisget.model.FleetOccurrence;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FleetOccurrenceRepository extends JpaRepository<FleetOccurrence, String> {
-}
